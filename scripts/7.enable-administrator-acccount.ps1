56cb10df72f8ff849d8c00f0f1e6ebf07aa143ef
Cleber Baldan Junior <cleber.baldan@gmail.com>
2020-01-22 08:08:52 -0600

    Improved script 7, found W10 bug and workaround :)

--- scripts/7.enable-administrator-acccount.ps1
+++ scripts/7.enable-administrator-acccount.ps1
@@ -1,3 +1,10 @@
 # For increased security, I enable the built-in administrator
 # account in Windows and demote the user account to Standard user
-Get-LocalUser -Name "Administrator" | Enable-LocalUser
+
+$adminUser = Get-LocalUser -Name "Admin*"
+$pwd = ConvertTo-SecureString "*123" -AsPlainText -Force
+
+# Thanks so some nefarious W10 bug, the password has to be set
+# in a different statement, after the account has been enabled
+Enable-LocalUser $adminUser
+Set-LocalUser $adminUser -Password $pwd
