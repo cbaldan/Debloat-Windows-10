782ee9c9524ecaeeb6fa16c75d7f831726fa3266
Cleber Baldan Junior <cleber.baldan@gmail.com>
2020-01-24 09:10:37 -0600

    Restart PC lib improved

--- lib/restart-pc-popup.psm1
+++ lib/restart-pc-popup.psm1
@@ -1,15 +1,14 @@
+Add-Type -AssemblyName System.Windows.Forms
 
 function RestartComputerPopup() {
-	$caption = "Reboot"
-	$message = "Reboot computer now?"
-	[int]$defaultChoice = 0
-	$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", "Yes"
-	$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", "No"
-	$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
-	$choiceRTN = $host.ui.PromptForChoice($caption,$message, $options, $defaultChoice)
 
-	if ( $choiceRTN -ne 1 )
-	{
-	   Restart-Computer -Force
+	$msgBoxInput =  [System.Windows.Forms.MessageBox]::Show('Would you like to reboot the PC now?','Reboot','YesNo','Question')
+
+	switch  ($msgBoxInput) {
+
+	'Yes' {
+		Restart-Computer -Force
 	}
+  }
+
 }
