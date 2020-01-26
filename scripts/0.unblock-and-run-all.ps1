57a4d039c2405b14f095d48df25d50ca5d83541f
Cleber Baldan Junior <cleber.baldan@gmail.com>
2020-01-26 02:54:29 -0600

    Fixed typo in script 0

--- scripts/0.unblock-and-run-all.ps1
+++ scripts/0.unblock-and-run-all.ps1
@@ -7,6 +7,6 @@ ls -Recurse *.ps*1 | Unblock-File
 
 # This is a workaround to what seem to be a PShell bug
 # The unblocking is only effective when called by a different script
-&($PSScriptRoot+"\0.invoke-all.ps1")
+&($PSScriptRoot+"\1.invoke-all.ps1")
 
 LogoffPopup
