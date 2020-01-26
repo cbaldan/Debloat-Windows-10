66492a1a72e8ccafb8ac55d07f90a64eafd2c150
Cleber Baldan Junior <cleber.baldan@gmail.com>
2020-01-26 02:51:42 -0600

    Created new script 0, renamed all

--- scripts/1.invoke-all.ps1
+++ scripts/1.invoke-all.ps1
@@ -18,24 +18,22 @@ Write-Output ">> Stopping Windows Update Service"$lineSeparator
 Stop-Service wuauserv
 
 Write-Output ">> Disabling services"$lineSeparator
-&($PSScriptRoot+"\1.disable-services.ps1")
+&($PSScriptRoot+"\2.disable-services.ps1")
 
 Write-Output ">> Optmizing Windows Update"$lineSeparator
-&($PSScriptRoot+"\2.optimize-windows-update.ps1")
+&($PSScriptRoot+"\3.optimize-windows-update.ps1")
 
 Write-Output ">> Removing OneDrive"$lineSeparator
-&($PSScriptRoot+"\3.remove-onedrive.ps1")
+&($PSScriptRoot+"\4.remove-onedrive.ps1")
 
 Write-Output ">> Removing Default Apps"$lineSeparator
-&($PSScriptRoot+"\4.remove-default-apps.ps1")
+&($PSScriptRoot+"\5.remove-default-apps.ps1")
 
 Write-Output ">> Optimizing User Interface"$lineSeparator
-&($PSScriptRoot+"\5.optimize-user-interface.ps1")
+&($PSScriptRoot+"\6.optimize-user-interface.ps1")
 
 Write-Output ">> Optimizing Start Menu"$lineSeparator
-&($PSScriptRoot+"\6.unbloat-start-menu.ps1")
+&($PSScriptRoot+"\7.unbloat-start-menu.ps1")
 
 Write-Output ">> Enabling the administrator account"$lineSeparator
-&($PSScriptRoot+"\7.enable-administrator-acccount.ps1")
-
-LogoffPopup
+&($PSScriptRoot+"\8.enable-administrator-acccount.ps1")
