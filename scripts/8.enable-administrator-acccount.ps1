01d6559b2ebb52b2949911d837be36a64148d7a0
Cleber Baldan Junior <cleber.baldan@gmail.com>
2020-01-27 05:28:08 -0600

    Added msgbox and inputbox to admin script

--- scripts/8.enable-administrator-acccount.ps1
+++ scripts/8.enable-administrator-acccount.ps1
@@ -5,10 +5,19 @@
 # For increased security, I enable the built-in Administrator
 # account in Windows and demote the user account to Standard User
 
-$adminUser = Get-LocalUser -Name "Admin*"
-$pwd = ConvertTo-SecureString "*123" -AsPlainText -Force
+Add-Type -AssemblyName Microsoft.VisualBasic
 
-# Thanks so some nefarious W10 bug, the password has to be set
-# in a different statement, after the account has been enabled
-Enable-LocalUser $adminUser
-Set-LocalUser $adminUser -Password $pwd
+$choice = [Microsoft.VisualBasic.Interaction]::MsgBox('Enable the Administrator account?', 'YesNo,SystemModal,Question', 'Enable admin')
+
+	switch  ($choice) {
+	'Yes'  {
+        $pwd = [Microsoft.VisualBasic.Interaction]::InputBox('Enter the Administrator account password', 'Password', '*123')
+        $pwd = ConvertTo-SecureString pwd -AsPlainText -Force
+
+        # Thanks so some nefarious W10 bug, the password has to be set
+        # in a different statement, after the account has been enabled
+        $adminUser = Get-LocalUser -Name "Admin*"
+        Enable-LocalUser $adminUser
+        Set-LocalUser $adminUser -Password $pwd
+           }
+    }
