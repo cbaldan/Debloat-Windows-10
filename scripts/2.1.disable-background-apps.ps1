e296e4ca4ce1b8eefcfdd78de304a063c377e08e
Cleber Baldan Junior <cleber.baldan@gmail.com>
2020-01-26 22:10:17 -0600

    Disabled MS Edge pre-launch - #2.1

--- scripts/2.1.disable-background-apps.ps1
+++ scripts/2.1.disable-background-apps.ps1
@@ -4,4 +4,10 @@
 # ===========
 # Disables all background apps
 
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\force-mkdir.psm1
+
 New-ItemProperty HKCU:Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications -Name GlobalUserDisabled -PropertyType DWORD -Value 1
+
+# Disable Microsoft Edge pre-launch
+force-mkdir "HKLM:SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main"
+Set-ItemProperty "HKLM:SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main" "AllowPrelaunch" 0
