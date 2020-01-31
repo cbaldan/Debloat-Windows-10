af1eb779d965b6f2c46a7b8cd86947882e8af4b7
Cleber Baldan Junior <cleber.baldan@gmail.com>
2020-01-31 02:23:02 -0600

    Reordered scripts execution in #1

--- scripts/1.invoke-all.ps1
+++ scripts/1.invoke-all.ps1
@@ -19,14 +19,17 @@ Stop-Service wuauserv
 
 Print-Message-With-Banner("Starting Windows 10 Cleanup")
 
+&($PSScriptRoot+"\8.enable-administrator-acccount.ps1")
+&($PSScriptRoot+"\4.remove-onedrive.ps1")
+&($PSScriptRoot+"\6.1.cleanup-taskbar.ps1")
+&($PSScriptRoot+"\7.unbloat-start-menu.ps1")
+
 &($PSScriptRoot+"\2.disable-services.ps1")
 &($PSScriptRoot+"\2.1.disable-background-apps.ps1")
 &($PSScriptRoot+"\3.optimize-windows-update.ps1")
-&($PSScriptRoot+"\4.remove-onedrive.ps1")
+
 &($PSScriptRoot+"\6.optimize-user-interface.ps1")
-&($PSScriptRoot+"\6.1.cleanup-taskbar.ps1")
-&($PSScriptRoot+"\7.unbloat-start-menu.ps1")
-&($PSScriptRoot+"\8.enable-administrator-acccount.ps1")
+
 &($PSScriptRoot+"\5.remove-default-apps.ps1")
 &($PSScriptRoot+"\9.remove-onedrive-leftovers.ps1")
 
